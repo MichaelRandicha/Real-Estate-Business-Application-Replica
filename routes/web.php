7ceedb7750f50a12e2fb5c4349deb4d1a8f62d71
SubaruYashiro <michaell.randicha@gmail.com>
2017-10-30 18:20:08 +0700

    penghapusan text ga penting

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,6 @@
 
 Auth::routes();
 
-// Route::get('/', function () {
-//     return view('login.index');
-// });
-
-	// Route::get('/', function(){
- //    	return view('login.index');
- //    });
-
 Route::group(['middleware' => 'guest'], function() {
 	Route::get('/', 'HomeController@index');   
 });
@@ -76,12 +68,6 @@ Route::group(['middleware' => 'auth'], function() {
 	});
 
 });
-
-
-
-// Route::group(['middleware' => 'guest'], function() {
-//     return "Hello World";
-// });
 
 Route::match(['get', 'post'], 'password/reset', function(){
     return abort(404);
