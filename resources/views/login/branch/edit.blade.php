8f1a5914e72fc548cb75b2b0369e5800d96059a6
SubaruYashiro <michaell.randicha@gmail.com>
2017-10-30 18:27:12 +0700

    branch bisa diubah vp dan pnya

--- resources/views/login/branch/edit.blade.php
+++ resources/views/login/branch/edit.blade.php
@@ -55,6 +55,42 @@
 				</div>
 			</div>
 
+			<div class="form-group{{ $errors->has('president') ? ' has-error' : '' }}">
+				<label for="president" class="col-md-4 control-label">President</label>
+
+				<div class="col-md-6">
+					<select name="president" id="president" class="form-control">
+					    <option value="0" selected></option>
+					    <option value="1">Alpha</option>
+					    <option value="2">Bravo</option>
+					    <option value="3">Charlie</option>
+					</select>
+					@if ($errors->has('president'))
+					<span class="help-block">
+						<strong>{{ $errors->first('president') }}</strong>
+					</span>
+					@endif
+				</div>
+			</div>
+
+			<div class="form-group{{ $errors->has('vice') ? ' has-error' : '' }}">
+				<label for="vice" class="col-md-4 control-label">Vice President</label>
+
+				<div class="col-md-6">
+					<select name="vice" id="vice" class="form-control">
+					    <option value="0" selected></option>
+					    <option value="1">Alpha</option>
+					    <option value="2">Bravo</option>
+					    <option value="3">Charlie</option>
+					</select>
+					@if ($errors->has('vice'))
+					<span class="help-block">
+						<strong>{{ $errors->first('vice') }}</strong>
+					</span>
+					@endif
+				</div>
+			</div>
+
 			<div class="form-group">
 				<div class="col-md-6 col-md-offset-4">
 					<button type="submit" class="btn btn-outline-primary">
@@ -66,4 +102,37 @@
 		</form>
 	</div>
 </div>
+
+<script type="text/javascript">
+	$(document).ready(function(){
+		var president = document.getElementById("president");
+		var vice = document.getElementById("vice");
+
+		$('#president').on('change', function () {
+			var index = president.selectedIndex;
+			
+			var option = vice.children;
+
+			for (var i = 0; i < option.length; i++) {
+				option[i].removeAttribute("hidden");
+			}
+			if(president.value > 0){
+				option[index].setAttribute('hidden', '');
+			}
+		});
+
+		$('#vice').on('change', function () {
+			var index = vice.selectedIndex;
+			
+			var option = president.children;
+
+			for (var i = 0; i < option.length; i++) {
+				option[i].removeAttribute("hidden");
+			}
+			if(vice.value > 0){
+				option[index].setAttribute('hidden', '');				
+			}
+		});
+	});
+</script>
 @endsection
