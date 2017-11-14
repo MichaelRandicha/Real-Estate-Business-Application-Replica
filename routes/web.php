29e85280f1cd4c85892f3b034dc1ca481b1d56b8
SubaruYashiro <michaell.randicha@gmail.com>
2017-11-14 16:05:08 +0700

    hapus white space lagi

--- routes/web.php
+++ routes/web.php
@@ -28,37 +28,37 @@ Route::group(['middleware' => 'auth'], function() {
 
 	Route::group(['prefix' => 'branch'], function(){
 		Route::get('/', 'BranchController@index')->name('branch');
-		
+
 		Route::get('add', 'BranchController@add')->name('branch.add');
 		Route::post('add', 'BranchController@register')->name('branch.register');
-		
+
 		Route::get('view/{id}', 'BranchController@view')->name('branch.view');
 
 		Route::get('list', 'BranchController@list')->name('branch.list');
 
 		Route::get('edit/{id}', 'BranchController@edit')->name('branch.edit');
-		Route::post('edit/{id}', 'BranchController@change')->name('branch.change');	
+		Route::post('edit/{id}', 'BranchController@change')->name('branch.change');
 	});
 
 	Route::group(['prefix' => 'agent'], function(){
 		Route::get('/', 'AgentController@index')->name('agent');
-		
+
 		Route::get('add', 'AgentController@add')->name('agent.add');
 		Route::post('add', 'AgentController@register')->name('agent.register');
-		
+
 		Route::get('view/{id}', 'AgentController@view')->name('agent.view');
 
 		Route::get('list', 'AgentController@list')->name('agent.list');
 
 		Route::get('edit/{id}', 'AgentController@edit')->name('agent.edit');
 		Route::post('edit/{id}', 'AgentController@change')->name('agent.change');
-		
+
 		Route::get('edit/{id}/status/change', 'AgentController@changeStatus')->name('agent.changestatus');
 	});
 
 	Route::group(['prefix' => 'closing'], function(){
 		Route::get('/', 'ClosingController@index')->name('closing');
-		
+
 		Route::get('add', 'ClosingController@add')->name('closing.add');
 		Route::post('add', 'ClosingController@register')->name('closing.register');
 
@@ -72,7 +72,7 @@ Route::group(['middleware' => 'auth'], function() {
 		Route::get('commission', 'ReportController@commission')->name('report.commission');
 		Route::get('closing', 'ReportController@closing')->name('report.closing');
 		Route::get('point', 'ReportController@point')->name('report.point');
-		
+
 		Route::get('branch/list/', 'ReportController@branchList')->name('report.branch.list');
 		Route::get('commission/list/', 'ReportController@commissionList')->name('report.commission.list');
 		Route::get('closing/list/', 'ReportController@closingList')->name('report.closing.list');
