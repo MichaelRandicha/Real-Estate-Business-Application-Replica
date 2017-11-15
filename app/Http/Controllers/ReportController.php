ca7593917d61c8391017407a4f74a9d99b084b07
SubaruYashiro <michaell.randicha@gmail.com>
2017-11-15 21:24:18 +0700

    merapikan codingan

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -11,83 +11,94 @@ class ReportController extends Controller
 {
     public function branch(Request $request){
         $cabangs = Cabang::all();
-        if(!empty($request->dateFrom) || !empty($request->dateTo) || !empty($request->branch)){
-            $this->validate($request,[
-                'dateFrom' => 'required|date',
-                'dateTo' => 'required|date',
-                'branch' => 'required']);
-            $no = 5;
-            if(!empty($request->no)){
-                $no = $request->no;
-            }
-            $branchs = AgentClosing::join('closings', 'agent_closing.closing_id', '=', 'closings.id')
-            ->where('closings.tanggal', '>=', $request->dateFrom)
-            ->where('closings.tanggal', '<=', $request->dateTo)
-            ->where('agent_closing.cabang_id', '=', $request->branch)
-            ->orderBy('closings.tanggal')
-            ->orderBy('agent_closing.closing_id')
-            ->paginate($no);
-            $branchs->appends(['dateFrom' => $request->dateFrom, 'dateTo' => $request->dateTo, 'no' => $no,'branch' => $request->branch]);
-            return view('report.branch', compact('cabangs', 'branchs'));
-        }else{
+        
+        if(empty($request->dateFrom) || empty($request->dateTo) || empty($request->branch)){
             return view('report.branch', compact('cabangs'));
         }
+
+        $this->validate($request,[
+            'dateFrom' => 'required|date',
+            'dateTo' => 'required|date',
+            'branch' => 'required']);
+
+        $no = 5;
+        if(!empty($request->no)){
+            $no = $request->no;
+        }
+        
+        $branchs = AgentClosing::join('closings', 'agent_closing.closing_id', '=', 'closings.id')
+        ->where('closings.tanggal', '>=', $request->dateFrom)
+        ->where('closings.tanggal', '<=', $request->dateTo)
+        ->where('agent_closing.cabang_id', '=', $request->branch)
+        ->orderBy('closings.tanggal')
+        ->orderBy('agent_closing.closing_id')
+        ->paginate($no);
+        $branchs->appends(['dateFrom' => $request->dateFrom, 'dateTo' => $request->dateTo, 'no' => $no,'branch' => $request->branch]);
+        
+        return view('report.branch', compact('cabangs', 'branchs'));
     }
 
     public function branchList(Request $request){
         if(empty($request->dateFrom) || empty($request->dateTo) || empty($request->branch)){
             return redirect('report/branch');
         }
+        
         $branchs = AgentClosing::join('closings', 'agent_closing.closing_id', '=', 'closings.id')
-            ->where('closings.tanggal', '>=', $request->dateFrom)
-            ->where('closings.tanggal', '<=', $request->dateTo)
-            ->where('agent_closing.cabang_id', '=', $request->branch)
-            ->orderBy('closings.tanggal')
-            ->orderBy('agent_closing.closing_id')
-            ->get();
+        ->where('closings.tanggal', '>=', $request->dateFrom)
+        ->where('closings.tanggal', '<=', $request->dateTo)
+        ->where('agent_closing.cabang_id', '=', $request->branch)
+        ->orderBy('closings.tanggal')
+        ->orderBy('agent_closing.closing_id')
+        ->get();
+        
         return view('report.list.branch', compact('branchs'));
     }
 
     public function commission(Request $request){
-        if(!empty($request->dateFrom) || !empty($request->dateTo) || !empty($request->filter)){
-            $this->validate($request,[
-                'dateFrom' => 'required|date',
-                'dateTo' => 'required|date']);
-            $no = 5;
-            if(!empty($request->no)){
-                $no = $request->no;
-            }
-            $filter = "";
-            if($request->filter == "agent"){
-                $filter = "agent_id";
-            }else if($request->filter == "branch"){
-                $filter = "cabang_id";
-            }
-            $commissions = AgentClosing::groupBy('agent_closing.'.$filter)
-            ->join('closings', 'agent_closing.closing_id', '=', 'closings.id')
-            ->where('closings.tanggal', '>=', $request->dateFrom)
-            ->where('closings.tanggal', '<=', $request->dateTo)
-            ->selectRaw('*, sum(komisi) as total_komisi')
-            ->orderBy('total_komisi', 'desc')
-            ->paginate($no);
-
-            $commissions->appends(['dateFrom' => $request->dateFrom, 'dateTo' => $request->dateTo, 'no' => $no,'filter' => $request->filter]);
-            return view('report.commission', compact('commissions'));
-        }else{
+        if(empty($request->dateFrom) || empty($request->dateTo) || empty($request->filter)){
             return view('report.commission');
         }
-    }
-
-    public function commissionList(Request $request){
-        if(empty($request->dateFrom) || empty($request->dateTo) || empty($request->filter)){
-            return redirect('report/commission');
+        
+        $this->validate($request,[
+            'dateFrom' => 'required|date',
+            'dateTo' => 'required|date']);
+        
+        $no = 5;
+        if(!empty($request->no)){
+            $no = $request->no;
         }
+        
         $filter = "";
         if($request->filter == "agent"){
             $filter = "agent_id";
         }else if($request->filter == "branch"){
             $filter = "cabang_id";
         }
+
+        $commissions = AgentClosing::groupBy('agent_closing.'.$filter)
+        ->join('closings', 'agent_closing.closing_id', '=', 'closings.id')
+        ->where('closings.tanggal', '>=', $request->dateFrom)
+        ->where('closings.tanggal', '<=', $request->dateTo)
+        ->selectRaw('*, sum(komisi) as total_komisi')
+        ->orderBy('total_komisi', 'desc')
+        ->paginate($no);
+        $commissions->appends(['dateFrom' => $request->dateFrom, 'dateTo' => $request->dateTo, 'no' => $no,'filter' => $request->filter]);
+        
+        return view('report.commission', compact('commissions'));
+    }
+
+    public function commissionList(Request $request){
+        if(empty($request->dateFrom) || empty($request->dateTo) || empty($request->filter)){
+            return redirect('report/commission');
+        }
+
+        $filter = "";
+        if($request->filter == "agent"){
+            $filter = "agent_id";
+        }else if($request->filter == "branch"){
+            $filter = "cabang_id";
+        }
+
         $commissions = AgentClosing::groupBy('agent_closing.'.$filter)
         ->join('closings', 'agent_closing.closing_id', '=', 'closings.id')
         ->where('closings.tanggal', '>=', $request->dateFrom)
@@ -99,32 +110,36 @@ class ReportController extends Controller
     }
 
     public function point(Request $request){
-        if(!empty($request->dateFrom) || !empty($request->dateTo) || !empty($request->filter)){
-            $this->validate($request,[
-                'dateFrom' => 'required|date',
-                'dateTo' => 'required|date']);
-            $no = 5;
-            if(!empty($request->no)){
-                $no = $request->no;
-            }
-            $points = AgentClosing::groupBy('agent_closing.agent_id')
-            ->join('closings', 'agent_closing.closing_id', '=', 'closings.id')
-            ->where('closings.tanggal', '>=', $request->dateFrom)
-            ->where('closings.tanggal', '<=', $request->dateTo)
-            ->selectRaw('*, sum(point) as total_point')
-            ->orderBy('total_point', 'desc')
-            ->paginate($no);
-            $points->appends(['dateFrom' => $request->dateFrom, 'dateTo' => $request->dateTo, 'no' => $no]);
-            return view('report.point', compact('points'));
-        }else{
+        if(empty($request->dateFrom) || empty($request->dateTo) || empty($request->filter)){
             return view('report.point');
         }
+
+        $this->validate($request,[
+            'dateFrom' => 'required|date',
+            'dateTo' => 'required|date']);
+        
+        $no = 5;
+        if(!empty($request->no)){
+            $no = $request->no;
+        }
+
+        $points = AgentClosing::groupBy('agent_closing.agent_id')
+        ->join('closings', 'agent_closing.closing_id', '=', 'closings.id')
+        ->where('closings.tanggal', '>=', $request->dateFrom)
+        ->where('closings.tanggal', '<=', $request->dateTo)
+        ->selectRaw('*, sum(point) as total_point')
+        ->orderBy('total_point', 'desc')
+        ->paginate($no);
+        $points->appends(['dateFrom' => $request->dateFrom, 'dateTo' => $request->dateTo, 'no' => $no]);
+        
+        return view('report.point', compact('points'));
     }
 
     public function pointList(Request $request){
         if(empty($request->dateFrom) || empty($request->dateTo)){
             return redirect('report/point');
         }
+
         $points = AgentClosing::groupBy('agent_closing.agent_id')
         ->join('closings', 'agent_closing.closing_id', '=', 'closings.id')
         ->where('closings.tanggal', '>=', $request->dateFrom)
@@ -132,39 +147,45 @@ class ReportController extends Controller
         ->selectRaw('*, sum(point) as total_point')
         ->orderBy('total_point', 'desc')
         ->get();
+
         return view('report.list.point', compact('points'));
     }
 
     public function closing(Request $request){
-        if(!empty($request->dateFrom) || !empty($request->dateTo)){
-            $this->validate($request,[
-                'dateFrom' => 'required|date',
-                'dateTo' => 'required|date']);
-            $no = 5;
-            if(!empty($request->no)){
-                $no = $request->no;
-            }
-            $closings = Closing::where('tanggal', '>=', $request->dateFrom)
-            ->where('tanggal', '<=', $request->dateTo)
-            ->orderBy('tanggal')
-            ->orderBy('id')
-            ->paginate($no);
-            $closings->appends(['dateFrom' => $request->dateFrom, 'dateTo' => $request->dateTo, 'no' => $no]);
-            return view('report.closing', compact('closings'));
-        }else{
+        if(empty($request->dateFrom) || empty($request->dateTo)){
             return view('report.closing');
         }
+        
+        $this->validate($request,[
+            'dateFrom' => 'required|date',
+            'dateTo' => 'required|date']);
+        
+        $no = 5;
+        if(!empty($request->no)){
+            $no = $request->no;
+        }
+        
+        $closings = Closing::where('tanggal', '>=', $request->dateFrom)
+        ->where('tanggal', '<=', $request->dateTo)
+        ->orderBy('tanggal')
+        ->orderBy('id')
+        ->paginate($no);
+        $closings->appends(['dateFrom' => $request->dateFrom, 'dateTo' => $request->dateTo, 'no' => $no]);
+        
+        return view('report.closing', compact('closings'));
     }
 
     public function closingList(Request $request){
         if(empty($request->dateFrom) || empty($request->dateTo)){
             return redirect('report/closing');
         }
+        
         $closings = Closing::where('tanggal', '>=', $request->dateFrom)
         ->where('tanggal', '<=', $request->dateTo)
         ->orderBy('tanggal')
         ->orderBy('id')
         ->get();
+        
         return view('report.list.closing', compact('closings'));
     }
 }
